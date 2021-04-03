5d6a25a9ae8c2abb35b92c4fa574bf1a9fb159f2
abba-ijah <81701885+abba-ijah@users.noreply.github.com>
2021-04-04 00:56:56 +0100

    Add files via upload

---
+++ Section 2 of CSC 104 assignment.py
@@ -0,0 +1,45 @@
+#1.	Write a short Python code to display the name of your name & department
+print("Name : Abba \nDepartment : Computer Science")
+
+#2.	Assign the string "Boo!" to the variable scare and print the variable.
+scare = "Boo!"
+print(scare)
+
+
+#3.	Add two variables and assign the result to a third variable. print all the variable names.
+variable1 = 5
+variable2 = 7
+variable3 = variable1 + variable2
+print("Var 1 : ", variable1, "\nVar 2 : ", variable2, "\n", variable3)
+
+
+#4.	Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
+x = 12
+z = 0.5
+b = x / z
+print(b)
+
+
+#5.	Assign the sum of two numbers to a variable and print the variable.
+e = 8
+t = 9
+g = e + t
+print(g)
+
+
+#6.	Write the code that finds the remainder when 8 is divided by 3
+e = 8
+t = 9
+g = e + t
+print(g)
+
+
+#7.	Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result.
+x = 2 * 4 * 4 + 2
+a = 2
+b = 4
+c = a * b
+d = b + a
+e = c * d
+print(e)
+
