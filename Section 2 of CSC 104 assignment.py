7b34d22b3dc1439d8828409ff152a064732f1fa3
abba-ijah <81701885+abba-ijah@users.noreply.github.com>
2021-04-04 18:14:22 +0100

    Add files via upload

---
+++ Section 2 of CSC 104 assignment.py
@@ -0,0 +1,44 @@
+# Question 1
+print("Name : Abba \nDepartment : Computer Science")
+
+# Question  2
+scare = "Boo!"
+print(scare)
+
+
+# Question 3
+variable1 = 5
+variable2 = 7
+variable3 = variable1 + variable2
+print("Var 1 : ", variable1, "\nVar 2 : ", variable2, "\n", variable3)
+
+
+# Question 4
+x = 12
+z = 0.5
+b = x / z
+print(b)
+
+
+# Question 5
+e = 8
+t = 9
+g = e + t
+print(g)
+
+
+# Question 6
+e = 8
+t = 3
+g = e % t
+print(g)
+
+
+# Question 7
+a = 2
+b = 4
+c = a * b
+d = b + a
+e = c * d
+print(e)
+
